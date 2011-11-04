652cab1d20d1079e775849a1b9eab022a560dd97
Nicolas Thiébaud <ni.thiebaud@gmail.com>
2011-11-04 22:35:08 +0100

    Sert statiquement tous les fichiers présents

--- chat.js
+++ chat.js
@@ -1,20 +1,34 @@
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
+var path = require('path');
 
 app.listen(8080);
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/chat.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+function handler (request, response) {
+  var filePath = '.' + request.url;
+    if (filePath == './')
+      filePath = './index.htm';
 
-    res.writeHead(200);
-    res.end(data);
-  });
+    path.exists(filePath, function(exists) {
+
+      if (exists) {
+        fs.readFile(filePath, function(error, content) {
+          if (error) {
+            response.writeHead(500);
+            response.end();
+          }
+          else {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(content, 'utf-8');
+          }
+        });
+      }
+      else {
+        response.writeHead(404);
+        response.end();
+      }
+    });
 }
 
 io.sockets.on('connection', function (socket) {
